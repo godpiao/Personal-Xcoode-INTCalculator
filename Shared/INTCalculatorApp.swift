4c40d4aea62dabec8dc96e51d450158b4d85c67d
godpiao <32320029+godpiao@users.noreply.github.com>
2021-03-20 14:37:53 +0800

    Initial Commit

---
+++ Shared/INTCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  INTCalculatorApp.swift
+//  Shared
+//
+//  Created by godpeo on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct INTCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
